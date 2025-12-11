5dc854170d8ebed835d6b1b8db09e848b94f1b4d
jessicaahm <jessica.ang@hashicorp.com>
2025-12-11 15:34:32 +0800

    added analysis for audit logs

---
+++ LOGGING/script.sh
@@ -0,0 +1,19 @@
+# Install Grafana
+sudo apt-get install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# Updates the list of available packages
+sudo apt-get update
+# Installs the latest OSS release:
+sudo apt-get install grafana
+
+# Verify Grafana is up and running
+ systemctl status grafana-server
+
+ # Start Grafana Services
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+
+# Enable Grafana to start at boot using systemd
+sudo systemctl enable grafana-server.service
